8f4ce2bcdb5248baa7b697328667565a683014dd
saidtimaev <said.timaev@stagiaire-elan.fr>
2024-04-02 16:57:48 +0200

    infos utilisateur

--- view/infosUtilisateur.php
+++ view/infosUtilisateur.php
@@ -3,11 +3,17 @@
     $utilisateur = $result["data"]['utilisateur']
 ?>
 
-<h1>Liste des posts de <?= $utilisateur->getPseudonyme()?></h1>
+<h1>Informations :</h2>
+<p><span>Pseudo : </span><?= $utilisateur->getPseudonyme()?></p>
+<p><span>Rôle : </span><?= ($utilisateur->getRole() == "ROLE_USER") ? "Utilisateur" : "Admin"?></p>
+<p><span>Email : </span><?= $utilisateur->getMail()?></p>
+<p><span>Date d'inscription : </span><?= $utilisateur->getDateInscription()->format('Y-m-d')?></p>
+<br>
+<h2>Liste des posts de <?= $utilisateur->getPseudonyme()?> :</h1>
 
 <?php
 foreach($posts as $post){ ?>
-    <p>"<?= $post->getTexte() ?>" par <?= $post->getUtilisateur()?> dans <?= $post->getTopic()?></p>
+    <p>"<?= $post->getTexte() ?>" dans <a href="#"><?= $post->getTopic()?></a> <a href="#">(<?= $post->getTopic()->getCategorie()->getNom()?>)</a></p>
 <?php } ?>
 
 
