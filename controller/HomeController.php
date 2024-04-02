847fa50af02ab575a69f47726a63d8e2a3aec64f
saidtimaev <said.timaev@stagiaire-elan.fr>
2024-04-02 11:58:58 +0200

    commencé affichage liste utilisateurs

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -21,7 +21,7 @@ class HomeController extends AbstractController implements ControllerInterface {
         $users = $manager->findAll(['register_date', 'DESC']);
 
         return [
-            "view" => VIEW_DIR."security/users.php",
+            "view" => VIEW_DIR."list/listUtilisateurs.php",
             "meta_description" => "Liste des utilisateurs du forum",
             "data" => [ 
                 "users" => $users 
---
+++ view/list/listUtilisateurs.php
@@ -0,0 +1,15 @@
+<?php
+    $categories = $result["data"]['categories']; 
+?>
+
+<h1>Liste des catégories</h1>
+
+<?php
+foreach($categories as $categorie ){ ?>
+    <p><a href="index.php?ctrl=topic&action=listTopicsByCategory&id=<?= $categorie->getId() ?>"><?= $categorie->getNom() ?></a></p>
+<?php } ?>
+
+<a href="index.php?ctrl=categorie&action=ajouterCategorieAffichage">Nouvelle catégorie</a>
+
+
+  
